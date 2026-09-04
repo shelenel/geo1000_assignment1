0e21d9d029b4b34036a1a38ff84a2bb94b502c5c
Shelene <shelenelow@proton.me>
2026-09-04 12:55:36 +0200

    Added my solutions to assignment
    - Added my student number and name
    - For triangle.py, I added an additional test for the triangle_area function, 
    - Added an extra test for point_in_triangle function with a different triangle 
    - Added extra code to consider clockwise and anti-clockwise orientations of triangles

--- circles.py
+++ circles.py
@@ -1,18 +1,34 @@
 # GEO1000 -- Assignment 1 -- Circle Intersections
-# Authors:
-# Studentnumbers:
+# Authors: Shelene Low
+# Studentnumbers: 6797687
 
 from math import sqrt
 
+# Epsilon
+eps = 1e-7
 
 def center_distance(x1, y1, x2, y2):
     """Calculates and returns the Euclidean distance between two center points."""
-    pass
-
+    euclid_dist = sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    return euclid_dist
 
 def classify_circles(x1, y1, r1, x2, y2, r2):
     """Classifies and prints the spatial relationship between two circles."""
-    pass
+    euclid_dist = center_distance(x1, y1, x2, y2)
+    if euclid_dist < eps and abs(r1 - r2) < eps:
+        print("Identical/coincident circles. There are infinite intersection points.")
+    elif euclid_dist < eps and abs(r1 - r2) >= eps:
+        print("Concentric and disjoint intersection. There are no intersection points.")
+    elif euclid_dist > (r1 + r2 + eps):
+        print("Separate (disjoint outside) circles. There are no intersection points.")
+    elif abs(euclid_dist - (r1 - r2)) < eps:
+        print("Two circles touch externally. There is 1 intersection point.")
+    elif abs(r1 - r2) < euclid_dist < (r1 + r2):
+        print("Two circles intersect. There are 2 intersection points.")
+    elif abs(euclid_dist- abs(r1 - r2)) < eps:
+        print("Two circles touch internally. There is 1 intersection point.")
+    else:
+        print("Two circles are nested inside each other. There are no intersection points.")
 
 
 # Test execution calls
--- triangle.py
+++ triangle.py
@@ -1,27 +1,69 @@
 # GEO1000 -- Assignment 1 -- Point-in-Triangle Test
-# Authors:
-# Studentnumbers:
+# Authors: Shelene Low
+# Studentnumbers: 6797687
 
 import math
 
+# Epsilon tolerance value
+eps = 1e-7
+
+# CW or CCW
+clockwise = counter_clockwise = False
 
 def cross_product(x1, y1, x2, y2, px, py):
     """Calculates 2D cross product determinant for orientation test."""
-    pass
-
+    crossproduct_determinant = (x2 - x1)*(py - y1) - (y2 - y1)*(px - x1)
+    return crossproduct_determinant
 
 def triangle_area(xa, ya, xb, yb, xc, yc):
     """Calculates and returns the area of triangle ABC."""
-    pass
-
+    area = 1/2 * abs(cross_product(xa, ya, xb, yb, xc, yc))
+    #print(area) # Used for test execution calls
+    return area
 
 def point_in_triangle(xa, ya, xb, yb, xc, yc, px, py):
     """Determines whether point P is inside, outside, or on the boundary of triangle ABC."""
-    pass
+
+    # Assumes one direction of triangle AB -> BC -> CA
+    ab = cross_product(xa,ya,xb,yb,px,py) # Cross product of points AB and P
+    bc = cross_product(xb,yb, xc, yc, px, py) # Cross product of points BC and P
+    ca = cross_product(xc, yc, xa, ya, px, py) # Cross product of points CA and P
+
+    # Consider CW and CCW directions
+    # This is not exactly necessary since vertex ordering is consistent here, but in case we have triangles with mixed orientations...
+    orientation = cross_product(xa, ya, xb, yb, xc, yc)
+    if orientation < -eps: # if determinant is negative, triangle is clockwise
+        ab, bc, ca = -ab, -bc, -ca # flip ordering of vertices
+
+    # Set boolean flag
+    has_zero = has_neg = has_pos = False
+
+    if (abs(ab) <= eps) or (abs(bc) <= eps) or (abs(ca) <= eps):
+        has_zero = True
+    if (ab < -eps) or (bc < -eps) or (ca < -eps):
+        has_neg = True
+    if (ab > eps) or (bc > eps) or (ca > eps):
+        has_pos = True
+
+    if has_pos and has_neg:
+        return("outside")
+    elif has_zero:
+        return("boundary")
+    else:
+        return("inside")
 
 
 # Test execution calls
-print(point_in_triangle(0, 0, 10, 0, 0, 10, 2, 2))   # Expected output: inside
-print(point_in_triangle(0, 0, 10, 0, 0, 10, 12, 5))  # Expected output: outside
-print(point_in_triangle(0, 0, 10, 0, 0, 10, 5, 0))   # Expected output: boundary
-print(point_in_triangle(0, 0, 10, 0, 0, 10, 0, 0))   # Expected output: boundary
+#print(point_in_triangle(0, 0, 10, 0, 0, 10, 2, 2))   # Expected output: inside
+#print(point_in_triangle(0, 0, 10, 0, 0, 10, 12, 5))  # Expected output: outside
+#print(point_in_triangle(0, 0, 10, 0, 0, 10, 5, 0))   # Expected output: boundary
+#print(point_in_triangle(0, 0, 10, 0, 0, 10, 0, 0))   # Expected output: boundary
+
+# Extra tests on another different triangle with different points
+#print(point_in_triangle(3, 4, 1, 1, 4, 1, 3, 2))     # Expected output: inside
+#print(point_in_triangle(3, 4, 1, 1, 4, 1, 2, -2))    # Expected output: outside
+#print(point_in_triangle(3, 4, 1, 1, 4, 1, 2, 1))     # Expected output: boundary
+
+# Test triangle_area function
+#triangle_area(0, 0, 10, 0, 0, 10)    # Expected output: 50.0
+#triangle_area(3, 4, 1, 1, 4, 1)      # Expected output: 4.5
